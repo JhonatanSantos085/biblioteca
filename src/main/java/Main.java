20dfc583e69e848258bb9f85f00f7c3a3a2ef2eb
Jhonatan Santos <jhonatansantos085@gmail.com>
2024-06-30 00:18:30 -0300

    Adcionei funcionalidade de cadastro de usuario

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int opcao;
+        do{
+            System.out.println("Escolha uma opção abaixo\n" +
+                    "[1] - Cadastrar funcionário\n" +
+                    "[2] - Cadastrar usuário\n" +
+                    "[3] - Cadastrar livro\n" +
+                    "[4] - Mostrar todos os dados do cadastro\n" +
+                    "[5] - Ir para locação \n"
+                    +"[0] - Sair\n"
+                    + "Digite aqui a opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao){
+                case 0:
+                    System.out.println("Encerrando programa!");
+                    break;
+                case 1:
+                    System.out.println("-_ Cadastrar Usuario _-");
+
+                    Usuario user = new Usuario();
+                    user.cadastrar();
+                    break;
+            }
+
+        }while(opcao != 0);
+        scanner.close();
+    }
+
+}
--- src/main/java/Usuario.java
+++ src/main/java/Usuario.java
@@ -1,5 +1,53 @@
+import java.util.Scanner;
 public class Usuario {
-    private String rua;
-    private int numero;
-    private String bairro;
+    private String nome;
+    private long codigo;
+    private long cpf;
+    private Endereco endereco;
+    Scanner scan = new Scanner(System.in);
+
+    public void cadastrar(){
+        System.out.print("Digite seu nome: ");
+        setNome(scan.nextLine());
+        this.nome = getNome();
+        System.out.print("Digite seu codigo de usuário: ");
+        setCodigo(scan.nextLong());
+        this.codigo = getCodigo();
+        System.out.print("Digite seu cpf: ");
+        setCpf(scan.nextLong());
+
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 }
